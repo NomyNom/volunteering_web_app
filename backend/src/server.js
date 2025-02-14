1c4c64ff58f51b6b2e952d11d94bec0bb7da5d0d
Minh Nguyen <nam08292002@gmail.com>
2025-02-13 18:37:56 -0600

    changed home page, added home button to profile page, added volunteering matching with dummy data, got started with server for backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,41 @@
 // backend/server.js
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const cors = require('cors');
+// const cors = require('cors');
 
 dotenv.config();
 
-const authRoutes = require('./components/Routes/Auth');
-const profileRoutes = require('./components/Routes/Profile');
-const eventRoutes = require('./components/Routes/Event');
+// const authRoutes = require('./components/Routes/Auth');
+// const profileRoutes = require('./components/Routes/Profile');
+// const eventRoutes = require('./components/Routes/Event');
 
 const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+// Middleware:
+// a concept where functions can be used to process incoming requests before they reach 
+// their final destination and handle outgoing responses before they are sent back to the client
 
-// Connect to MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+// app.use(express.json());
+// app.use(cors());
 
-// Mount routes
-app.use('/api/auth', authRoutes);
-app.use('/api/profile', profileRoutes);
-app.use('/api/events', eventRoutes);
+// // Connect to MongoDB
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// // Mount routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/profile', profileRoutes);
+// app.use('/api/events', eventRoutes);
+
+// Route
+app.get('/', (req, res) => {
+  res.json({mssg: 'Welcome to the app'})
+})
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT)
+})
